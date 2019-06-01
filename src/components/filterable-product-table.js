1854621d27f42f4339ad0c6055b4cf66da0eed3f
Guilherme Barbosa Pereira <guilherme.barbosapereira@outlook.com>
2019-06-01 20:01:08 -0300

    Add ProductTable component template

--- src/components/filterable-product-table.js
+++ src/components/filterable-product-table.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import SearchBar from './search-bar';
+import ProductTable from './product-table';
 
 export default class FilterableProductTable extends React.Component {
     render() {
         return (
             <div>
                 <SearchBar />
-                {/* <ProductTable /> */}
+                <ProductTable />
             </div>
         );
     }
---
+++ src/components/product-table.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class ProductTable extends React.Component {
+    render() {
+        return (
+            <table>
+                <tr>
+                    <th>Name</th>
+                    <th>Price</th>
+                </tr>
+            </table>
+        );
+    }
+}
