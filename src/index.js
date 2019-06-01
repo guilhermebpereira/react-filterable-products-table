7c69546e871be0676239f7ae26c2ccdd3caf412a
Guilherme Barbosa Pereira <guilherme.barbosapereira@outlook.com>
2019-06-01 19:44:33 -0300

    Fetch products table from mock.json file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
 class App extends React.Component {
+
+    componentWillMount() {
+        fetch('mock.json')
+            .then((response) => response.json())
+            .then((response) => console.log("response: ", response))
+            .catch((error) => console.log('error: ', error));
+    }
+
     render() {
         return (
             <h1>Hello World</h1>
