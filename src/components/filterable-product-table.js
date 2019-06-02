b38da6aa3b1b03bf0fafb8fc9c0c6f1d82f0e1e9
Guilherme Barbosa Pereira <guilherme.barbosapereira@outlook.com>
2019-06-02 11:01:45 -0300

    Filter stocked products when checkbox is checked

--- src/components/filterable-product-table.js
+++ src/components/filterable-product-table.js
@@ -7,15 +7,50 @@ export default class FilterableProductTable extends React.Component {
         super();
         this.state = {
             filterText: '',
-            isStockOnly: false
+            isStockOnly: false,
+            filteredProducts: []
         };
+
+        this.onChangeFilterText = this.onChangeFilterText.bind(this);
+        this.onChangeIsStockOnly = this.onChangeIsStockOnly.bind(this)
+    }
+
+    componentWillReceiveProps(props) {
+        if (props.products && props.products.length !== this.state.filteredProducts.length) {
+            this.setState({ filteredProducts: this.filterByName(props.products, this.state.filterText) });
+        } 
+    }
+
+    onChangeFilterText(event) {
+        this.setState({
+            filterText: event.target.value
+        });
+    }
+
+    onChangeIsStockOnly(event) {
+        this.setState({
+            isStockOnly: event.target.checked,
+            filteredProducts: event.target.checked ? this.filterStocked(this.state.filteredProducts) : this.filterByName(this.props.products, this.state.filterText)
+        });
+    }
+
+    filterStocked(products) {
+        return products.filter((product) => product.stocked);
+    }
+
+    filterByName(products, text) {
+        if (products && text && text.trim()) {
+            return products.filter((product) => product.name.includes(text));
+        } else {
+            return products;
+        }
     }
 
     render() {
         return (
             <div>
-                <SearchBar filterText={ this.state.filterText } isStockOnly={this.state.isStockOnly}/>
-                <ProductTable  products={ this.props.products }/>
+                <SearchBar filterText={ this.state.filterText } onChangeFilterText={this.onChangeFilterText} onChangeIsStockOnly={this.onChangeIsStockOnly} isStockOnly={this.state.isStockOnly}/>
+                <ProductTable  products={ this.state.filteredProducts }/>
             </div>
         );
     }
--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -4,8 +4,8 @@ export default class SearchBar extends React.Component {
     render() {
         return (
             <div>
-                <input type="search" value={this.props.filterText} placeholder="Search..."/>
-                <input type="checkbox" checked={this.props.isStockOnly} />
+                <input type="search" value={this.props.filterText} onChange={this.props.onChangeFilterText} placeholder="Search..."/>
+                <input type="checkbox" checked={this.props.isStockOnly} onChange={this.props.onChangeIsStockOnly} />
                 <label>Only show products in stock</label>
             </div>
         );
